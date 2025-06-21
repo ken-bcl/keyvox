1b908bfac25e142826cf999e26cda325f54e927e
Ken Okamoto <ken.okamoto@blockchainlock.io>
2025-06-21 21:51:17 +0900

    Update index.cjs
    fix bot room

--- KAI-bot/index.cjs
+++ KAI-bot/index.cjs
@@ -19,7 +19,8 @@ const client = new Client({
 const app = express();
 
 client.on('messageCreate', async (message) => {
-  if (message.author.bot) return;
+if (message.author.bot || message.webhookId) return;
+console.log('Sending to Chatbase:', message.content);
 
   try {
     const res = await axios.post(
